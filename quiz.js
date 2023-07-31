ca391c9296f4e6aa0c68a7e43778cb9c762afb53
Ashmeet670 <73339721+Ashmeet670@users.noreply.github.com>
2023-07-31 20:48:03 +0530

    quiz donbeee

--- quiz.js
+++ quiz.js
@@ -1,62 +1,92 @@
-const quizForm = document.getElementById('quizForm');
+const quizData = [
+    {
+        question: " What is the full form of ISRO?",
+        a: "International Space Research Organisation",
+        b: "Institute for Space Research and Observation",
+        c: "Indian Space Research Organisation",
+        d: "Indian Space and Research Observatory",
+        correct: "c",
+    },
+    {
+        question: " What is the full form of GSLV?",
+        a: "General Satellite Lifting Vehicle",
+        b: "Geosynchronous Satellite Launch Vehicle",
+        c: "Geosynchronous Satellite Lifting Vehicle",
+        d: "Geostar Satellite Launch Vehicle",
+        correct: "b",
+    },
+    {
+        question: " When was ISRO Established?",
+        a: "Aug 15, 1969",
+        b: "Jan 26, 1969",
+        c: "Aug 15, 1948",
+        d: "Jan 15, 1948",
+        correct: "a",
+    },
+    {
+        question: " ISRO is monitored directly under whom?",
+        a: "President",
+        b: "President",
+        c: "Prime Minister",
+        d: "none of the above",
+        correct: "c",
+    },
+];
 
+const quiz = document.getElementById("quiz");
+const answerElements = document.querySelectorAll(".answer");
+const questionElement = document.getElementById("question");
+const b_text = document.getElementById("Labelb");
+const c_text = document.getElementById("Labelc");
+const d_text = document.getElementById("Labeld");
+const a_text = document.getElementById("Labela");
+const submitButton = document.getElementById("submit");
 
+let currentQuiz = 0;
+let score = 0;
+let level = currentQuiz + 1;
 
+const deselectAnswers = () => {
+    answerElements.forEach((answer) => (answer.checked = false));
+};
 
-function getSelectedOption(question) {
-    const selectedOption = quizForm.elements[question].value;
-    return selectedOption;
-}
+const getSelectedOption = () => {
+    let answer;
+    answerElements.forEach((answerElement) => {
+        if (answerElement.checked) {
+            answer = answerElement.id;
+        }
+    });
+    return answer;
+};
 
+const loadQuiz = () => {
+    deselectAnswers();
+    const currentQuizData = quizData[currentQuiz];
+    questionElement.innerText = "Q" + level + ": " + currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+};
 
-function calculateScore(ans1,ans2,ans3,ans4,ans5,ans6,ans7,ans8,ans9,ans10,ans11,ans12,ans13,ans14,ans15){
-    score = 0
-    if(ans1 == "c"){
-        score += 1
+loadQuiz();
+
+submitButton.addEventListener("click", () => {
+    const answer = getSelectedOption();
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) score++;
+        console.log(answer + " answer")
+        console.log(quizData[currentQuiz].correct)
+        console.log(score + "score")
+        currentQuiz++;
+        level++
+        if (currentQuiz < quizData.length) loadQuiz();
+        else {
+            quiz.innerHTML = `
+              <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+              <button onclick="history.go(0)">Play Again</button>
+          `
+        }
     }
-    if(ans2 == "b"){
-        score += 1
-    }
-    if(ans3 == "a"){
-        score += 1
-    }
-    if(ans4 == "c"){
-        score += 1
-    }
-    if(ans5 == "b"){
-        score += 1
-    }
-
-
-    return score
-}
-
-quizForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    // Get the selected options for each question
-    
-    const ans1 = getSelectedOption('q1');
-    const ans2 = getSelectedOption('q2');
-    const ans3 = getSelectedOption('q3');
-    const ans4 = getSelectedOption('q4');
-    const ans5 = getSelectedOption('q5');
-
-    const ans6 = getSelectedOption('q6');
-    const ans7 = getSelectedOption('q7');
-    const ans8 = getSelectedOption('q8');
-    const ans9 = getSelectedOption('q9');
-    const ans10 = getSelectedOption('q10');
-    const ans11 = getSelectedOption('q11');
-    const ans12 = getSelectedOption('q12');
-    const ans13 = getSelectedOption('q13');
-    const ans14 = getSelectedOption('q14');
-    const ans15 = getSelectedOption('q15');
-
-
-    score = calculateScore(ans1,ans2,ans3,ans4,ans5,ans6,ans7,ans8,ans9,ans10,ans11,ans12,ans13,ans14,ans15)
-    console.log(score)
-}); 
-
-
-
+});
